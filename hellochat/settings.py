94cceba3baa0c60ba60aa882d928bdb04d4e982c
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2024-12-28 03:06:01 +0600

    extend auth user model

--- hellochat/settings.py
+++ hellochat/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'server',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
