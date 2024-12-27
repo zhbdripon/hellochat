997a35ab48dd31f99c432d789728baaabc204f04
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2024-12-28 02:25:53 +0600

    project initialization

---
+++ hellochat/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for hellochat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hellochat.settings')
+
+application = get_asgi_application()
