5c18309a0e2b5a8b2bd67e13f3ad891f354ccc49
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2024-12-28 12:46:41 +0600

    add server model to admin

--- server/admin.py
+++ server/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Channel, Server, Category
+
 # Register your models here.
+
+admin.site.register(Channel)
+admin.site.register(Server)
+admin.site.register(Category)
