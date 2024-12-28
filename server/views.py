9da11fd74fa1ac94f8dee1bf3fe395754d99e7ab
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2024-12-28 21:48:36 +0600

    add filter on server list endpoint

--- server/views.py
+++ server/views.py
@@ -3,16 +3,28 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import Server
 from .serializers import ServerSerializer
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 
 class ServerViewSet(viewsets.ViewSet):
+
+    queryset = Server.objects.all()
+    permission_classes = [IsAuthenticated]
+
     def list(self, request):
         category = request.query_params.get('category')
+        by_user = request.query_params.get('by_user') == 'true'
+        limit = request.query_params.get('limit')
 
-        if category is None:
-            servers = Server.objects.all()
-        else:
-            servers = Server.objects.filter(category=category).all()
+        if category:
+            self.queryset = self.queryset.filter(category__name=category)
 
-        serializer = ServerSerializer(servers, many=True)
+        if by_user:
+            self.queryset = self.queryset.filter(owner=request.user)
+
+        if limit:
+            self.queryset = self.queryset[:int(limit)]
+
+        serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
