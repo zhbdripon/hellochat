501eb155dfedba36d1a972225f209453565614d0
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2024-12-30 19:00:09 +0600

    save chat message on database

--- webchat/consumer.py
+++ webchat/consumer.py
@@ -1,6 +1,11 @@
 from channels.generic.websocket import JsonWebsocketConsumer
 from asgiref.sync import async_to_sync
 
+from .models import Conversation, Message
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class WebChatConsumer(JsonWebsocketConsumer):
     def __init__(self, *args, **kwargs):
@@ -9,25 +14,37 @@ class WebChatConsumer(JsonWebsocketConsumer):
         self.user = None
 
     def connect(self):
-        self.channel_id = self.scope['url_route']['kwargs']['channel_id']
-        self.server_id = self.scope['url_route']['kwargs']['server_id']
+        self.channel_id = self.scope["url_route"]["kwargs"]["channel_id"]
+        self.server_id = self.scope["url_route"]["kwargs"]["server_id"]
+        self.user = User.objects.get(pk=1)  # static user for now
         self.accept()
         async_to_sync(self.channel_layer.group_add)(
-            self.channel_id,
-            self.channel_name
-        )
+            self.channel_id, self.channel_name)
 
     def receive(self, text_data):
+        conversation, created = Conversation.objects.get_or_create(
+            channel=self.channel_id)
+        message = Message.objects.create(
+            conversation=conversation, content=text_data, author=self.user
+        )
+
         async_to_sync(self.channel_layer.group_send)(
             self.channel_id,
             {
-                'type': 'chat_message',
-                'message': text_data,
-            }
+                "type": "chat_message",
+                "message": {
+                    "content": message.content,
+                    "id": message.id,
+                    "author": self.user.username,
+                    "timestamp": message.created_at.isoformat(),
+                },
+            },
         )
 
     def chat_message(self, event):
         self.send_json(event)
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.channel_id, self.channel_name)
+        super().disconnect(close_code)
--- webchat/models.py
+++ webchat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+
+class Conversation(models.Model):
+    channel = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE)  # noqa
+    created_at = models.DateTimeField(auto_now_add=True)
