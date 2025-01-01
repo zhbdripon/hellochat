e263d991de8a16d44d9b63cfe0e2687aa0f8200e
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2025-01-01 17:42:49 +0600

    add api client service

---
+++ frontend/src/services/apiClient.ts
@@ -0,0 +1,44 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export interface ListApiResponse<T> {
+  count: number;
+  results: T[];
+  next?: string | null;
+  previous?: string | null;
+}
+
+const axiosInstance = axios.create({
+  baseURL: "http://172.28.28.49:8000",
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access");
+  if (token) {
+    config.headers.Authorization = `JWT ${token}`;
+  }
+  return config;
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<T>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = () => {
+    return axiosInstance.get<T>(this.endpoint).then((res) => res.data);
+  };
+
+  post = (data: any) => {
+    return axiosInstance.post<T>(this.endpoint, data).then((res) => res.data);
+  };
+}
+
+export default APIClient;
