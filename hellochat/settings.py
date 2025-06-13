fdb7893b14ca1be0c4641a4d1c1fec0b5d6c20b0
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2025-06-13 23:31:19 +0600

    log: show email on console on development

--- hellochat/settings.py
+++ hellochat/settings.py
@@ -231,9 +231,13 @@ if DEBUG:
     ]
 
 # mailing
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
